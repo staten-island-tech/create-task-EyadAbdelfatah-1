b211f7e3d8f3de378a2670f184f1a2660cb295dc
mellowed24 <eyada5@nycstudents.net>
2025-01-12 13:34:14 -0500

    Update main.js

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,9 +1,7 @@
 import "./style.css";
 const DOMSelectors = {
-  card: document.querySelector("#card"),
-  result: document.querySelector("#result"),
-  bet: document.querySelector("#search-input"),
-  bet_button: document.querySelector("#submit"),
+  ai: document.querySelector("#ai"),
+  user: document.querySelector("#user")
 };
 async function drawCard() {
   try {
@@ -16,19 +14,26 @@ async function drawCard() {
     response = await fetch(
       `https://deckofcardsapi.com/api/deck/${id}/draw/?count=52`
     );
-
+    console.log(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=52`);
     result = await response.json();
-
     return result;
-  } catch (error) {
-    console.log("the api is poopy");
-  }
+  } catch (error) {}
 }
-async function convert() {
+
+async function gameLogic() {
+  let user_win_count = 0;
+  let ai_win_count = 0;
+
+  let user_balance = 0;
+  let ai_balance = 0;
+
   let deck = await drawCard();
-  convert(deck);
-  deck.cards.forEach((card) => {
-    if (card.value === "JACK") {
+
+  let user_card = deck.cards[i];
+  let ai_card = deck.cards[i + 1];
+
+  deck.cards.forEach(card => {
+    if (card.value === "JACKS") {
       card.value = 11;
     } else if (card.value === "QUEEN") {
       card.value = 12;
@@ -38,70 +43,30 @@ async function convert() {
       card.value = 1;
     }
   });
-  return deck;
-}
-let convertedDeck = await convert();
-let i = 0;
-async function gameLogic() {
-  let win;
-
-  let user_card = convertedDeck.cards[i];
-  let ai_card = convertedDeck.cards[i + 1];
-  i += 2;
-  console.log(convertedDeck);
-  DOMSelectors.card.insertAdjacentHTML(
+  DOMSelectors.ai.insertAdjacentHTML(
     "afterbegin",
-    `<div id="ai-card"><h2>This is you opponents card</h2><img src="${ai_card.image}" alt="${ai_card.value}"></div>
-    <div id="ai-card"><h2>This is your card</h2><img src="${user_card.image}" alt="${user_card.value}"></div>`
+    `<div id="ai-card"><img src="${ai_card.image}" alt=""><p>${ai_card.value}</p></div>`
   );
-
-  if (Number(user_card.value) > Number(ai_card.value)) {
-    DOMSelectors.result.insertAdjacentHTML(
-      "beforeend",
-      `<h2 class="">You win</h2>`
-    );
-    win = true;
-    return win;
-  } else if (Number(user_card.value) < Number(ai_card.value)) {
-    DOMSelectors.result.insertAdjacentHTML(
-      "beforeend",
-      `<h2 class="">You lose</h2>`
-    );
-    win = false;
-    return win;
-  } else {
-    DOMSelectors.result.insertAdjacentHTML(
-      "beforeend",
-      `<h2 class="">draw</h2>`
-    );
-    win = false;
-    return win;
+  while (user_win_count < 2 || ai_win_count < 2) {
+    if (Number(user_card.value) > Number(ai_card.value)) {
+      user_win_count++;
+    } else {
+      ai_win_count++;
+    }
   }
 }
+gameLogic();
 
-let user_balance = 100;
-let ai_balance = 100;
-function betting(user_bet, win) {
-  console.log(user_bet);
-  user_bet = Number(user_bet);
-  console.log(typeof user_bet);
-  if (user_balance < user_bet) {
-    console.log("unc");
-  } else if (user_balance >= user_bet) {
+function betting(user_bet) {
+  if (user_balance < bet || bet != Number) {
+    console.log("Please bet less/bet a valid number");
+  } else if (user_balance > bet || bet == Number) {
     if (win == true) {
-      console.log("sdfh");
       user_balance += user_bet;
       ai_balance -= user_bet;
     } else {
-      console.log("sdfh");
       ai_balance += user_bet;
       user_balance -= user_bet;
     }
   }
-  console.log(user_balance);
 }
-DOMSelectors.bet_button.addEventListener("click", async function () {
-  event.preventDefault();
-  const x = await gameLogic();
-  betting(DOMSelectors.bet.value, x);
-});
